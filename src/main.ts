a5d810caece2ee7aab9c1e242f288976225d8e9a
kraken <kraken@ex.ua>
2016-10-08 14:53:31 +0300

    lal

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import './assets/style.css'
+
 import { AppModule } from './app/app.module';
 
+
+import './styles.css'
 // enableProdMode(); 
+
+
 
 platformBrowserDynamic().bootstrapModule(AppModule);
